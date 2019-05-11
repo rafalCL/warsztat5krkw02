313144605c9fb215117da8efd20524ec03c7005c
rafalcl <rafalclab@gmail.com>
2019-05-11 14:30:18 +0200

    Initialize Spring project

---
+++ src/main/java/pl/coderslab/warsztat5krkw02/app/AppConfig.java
@@ -0,0 +1,11 @@
+package pl.coderslab.warsztat5krkw02.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan("pl.coderslab.warsztat5krkw02")
+@EnableWebMvc
+public class AppConfig {
+}
---
+++ src/main/java/pl/coderslab/warsztat5krkw02/controller/Home.java
@@ -0,0 +1,14 @@
+package pl.coderslab.warsztat5krkw02.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Home {
+    @GetMapping("")
+    @ResponseBody
+    public String main(){
+        return "Działa!!! :)";
+    }
+}
